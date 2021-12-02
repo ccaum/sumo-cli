ba76f9f0eeb056feda1b5d2e36041614264359f3
Carl Caum <carl@carlcaum.com>
2021-12-01 16:56:52 -0800

    Update descriptions of sumo help cmd

--- cmd/root.go
+++ cmd/root.go
@@ -30,16 +30,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "sumo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Sumo Logic CLI",
+	Long: `sumo is a Sumo Logic CLI and library for building Sumo Logic dashboards and
+applications as well as interacting with Sumo Logic platform capabilities.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -56,10 +49,6 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sumo-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
