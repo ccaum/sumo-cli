4ad6a9b38f588772a7d636afedc60735cc984051
Carl Caum <carl@carlcaum.com>
2021-12-06 11:59:05 -0800

    Fix variable Merge() function

--- sumoapp/variable.go
+++ sumoapp/variable.go
@@ -5,9 +5,29 @@ import (
 )
 
 func (v *variable) Merge(varObj *variable) error {
-	if err := mergo.Merge(v, varObj, mergo.WithOverride); err != nil {
+	newVar := varObj.Copy()
+
+	if err := mergo.Merge(newVar, v, mergo.WithOverride); err != nil {
+		return err
+	}
+
+	if err := mergo.Merge(v, newVar, mergo.WithOverride); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (v *variable) Copy() *variable {
+	return &variable{
+		Id:               v.Id,
+		Name:             v.Name,
+		DisplayName:      v.DisplayName,
+		DefaultValue:     v.DefaultValue,
+		SourceDefinition: v.SourceDefinition,
+		AllowMultiSelect: v.AllowMultiSelect,
+		IncludeAllOption: v.IncludeAllOption,
+		HideFromUI:       v.HideFromUI,
+		ValueType:        v.ValueType,
+	}
 }
