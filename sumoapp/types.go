c0fafa8efbeed00c4efd6f8461a3cf6b419c5040
nykh <nicholas.ykhuang@gmail.com>
2021-12-08 22:57:13 +0100

    Omit non-existing fields when loading

--- sumoapp/types.go
+++ sumoapp/types.go
@@ -136,13 +136,14 @@ type panel struct {
 	Text                                   string     `json:"text,omitempty"`
 }
 
+// SourceDefinition can take many different forms. So beside VariableSourceType all the fields are omitempty
 type sourceDefinition struct {
 	VariableSourceType string `json:"variableSourceType"`
-	Query              string `json:"query"`
-	Field              string `json:"field"`
-	Filter             string `json:"filter"`
-	Key                string `json:"key"`
-	Values             string `json:"values"`
+	Query              string `json:"query" yaml:"query,omitempty"`
+	Field              string `json:"field" yaml:"field",omitempty"`
+	Filter             string `json:"filter" yaml:"filter,omitempty"`
+	Key                string `json:"key" yaml:"key,omitempty"`
+	Values             string `json:"values" yaml:"values,omitempty"`
 }
 
 type variable struct {
